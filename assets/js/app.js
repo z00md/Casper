c1a1ccf2204ea813d126d420300a40167c074ae7
z00md <z00md.down@gmail.com>
2020-07-13 13:36:39 +0530

    Added new container for medium zoom for better image positioning.

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,7 +12,7 @@ function managePostImages(){
 // Add special class to zoomable images
 managePostImages();
 // Call medium zoom library on all imgs within post-content
-mediumZoom('.js-zoomable', { background: '#111' });
+mediumZoom('.js-zoomable', { container: "#medium-zoom-container", background: '#111', scrollOffset: 100 });
 // mediumZoom('.post-content .kg-image-card img', { background: '#111' });
 // mediumZoom('.post-content .kg-gallery-card img', { background: '#111' });
 // mediumZoom('.post-content .kg-gallery-card img', { background: '#111' });
