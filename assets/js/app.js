53dac11d34b8fded9ed2daecc789a886305aded5
z00md <z00md.down@gmail.com>
2020-07-16 20:27:32 +0530

    Added manual dark mode Remove auto-dark mode as it was interferring with manual mode Added postcss-nested for scss type nesting support

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,3 +16,36 @@ mediumZoom('.js-zoomable', { container: "#medium-zoom-container", background: '#
 // mediumZoom('.post-content .kg-image-card img', { background: '#111' });
 // mediumZoom('.post-content .kg-gallery-card img', { background: '#111' });
 // mediumZoom('.post-content .kg-gallery-card img', { background: '#111' });
+
+var currentTheme = window.localStorage.getItem("theme");
+if (currentTheme) {
+  toggleTheme(currentTheme);
+}
+
+$(".theme-toggle").on("click", function(evt) {
+  var attr = evt.currentTarget.getAttribute("data-theme");
+  var result;
+  if (attr) {
+    result = "light";
+  } else {
+    result = "dark"
+  }
+  toggleTheme(result);
+  window.localStorage.setItem("theme", result);
+});
+
+function toggleTheme(theme) {
+  var html = $("html");
+  var themeToggleButton = $(".theme-toggle");
+
+  // reset
+  $.each(themeToggleButton, function(idx, el){el.removeAttribute("data-theme")});
+  html.removeClass("dark");
+
+  // change only in case of dark
+  if (theme == "dark") {
+    $.each(themeToggleButton, function(idx, el){el.setAttribute("data-theme", "dark")});
+    // themeToggleButton.setAttribute("data-theme", "dark");
+    html.addClass("dark");
+  }
+}
--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,7 @@ const colorFunction = require('postcss-color-function');
 const cssnano = require('cssnano');
 const customProperties = require('postcss-custom-properties');
 const easyimport = require('postcss-easy-import');
+const nested = require('postcss-nested');
 
 const REPO = 'TryGhost/Casper';
 const REPO_READONLY = 'TryGhost/Casper';
@@ -50,6 +51,7 @@ function css(done) {
         src('assets/css/*.css', {sourcemaps: true}),
         postcss([
             easyimport,
+            nested,
             customProperties({preserve: false}),
             colorFunction(),
             autoprefixer(),
